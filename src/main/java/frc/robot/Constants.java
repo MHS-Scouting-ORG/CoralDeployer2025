dc583936961ba2f37a03058ca9e3b1a4c8f314a2
Joti Huynh <huynh.joti@gmail.com>
2025-01-14 20:25:20 -0800

    making intake code for algae

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,8 +12,20 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {
+public final class Constants { 
+   public static final double ALGAE_INTAKE_MAX_POWER = 0.1;
+    public static final double ALGAE_INTAKE_MIN_POWER = 0.05;
+    public static final double ALGAE_INTAKE_kP = 0.025;
+   
+   
+    
+    public static final double DEPLOY_SPEED = 0.1;
+    public static final boolean ALGAE_INTAKE_INVERT = false;
+    public static final double ALGAE_INTAKE_SPEED = 0.8;
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
+   
+  
+
   }
 }
--- src/main/java/frc/robot/subsystems/AlgaeIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/AlgaeIntakeSubsystem.java
@@ -4,12 +4,37 @@
 
 package frc.robot.subsystems;
 
+import frc.robot.Constants;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 
 public class AlgaeIntakeSubsystem extends SubsystemBase {
   /** Creates a new AlgaeIntakeSubsystem. */
-  public AlgaeIntakeSubsystem() {}
+  private SparkMax algaeIntake;
 
+  private double algaeIntakeRPM;
+  private double algaeIntakePosition;
+
+  public AlgaeIntakeSubsystem() {
+    algaeIntake = new SparkMax(0, MotorType.kBrushless);
+
+      configureSparkMAX(algaeIntake, Constants.ALGAE_INTAKE_INVERT);
+
+      SmartDashboard.putNumber("Algae Intake Speed", algaeIntake.get());
+  }
+  private void configureSparkMAX(SparkMax max, boolean reverse) {
+		SparkMaxConfig config = new SparkMaxConfig();
+    config.inverted(reverse).idleMode(IdleMode.kBrake);
+    max.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
