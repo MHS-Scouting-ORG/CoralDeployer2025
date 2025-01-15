4a49e15cad50316729092c01b2321c0f8b7a9849
John Carlo Villanueva <johncarlovillanueva2008@gmail.com>
2025-01-14 16:04:17 -1000

    made intake, outtake, and stop method for coral

--- src/main/java/frc/robot/subsystems/CoralIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralIntakeSubsystem.java
@@ -1,10 +1,28 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CoralIntakeSubsystem extends SubsystemBase {
-  /** Creates a new CorallIntakeSubsystem. */
-  public CoralIntakeSubsystem() {}
+  
+  private TalonFX coralIntake;
+
+  public CoralIntakeSubsystem() {
+    coralIntake = new TalonFX(9);
+  }
+
+  public void intake(double speed){ // speed = amount of voltage on motor
+    coralIntake.setVoltage(speed);
+  }
+
+  public void outtake(double speed){
+    coralIntake.setVoltage(-speed);
+  }
+
+  public void stop(){
+    coralIntake.setVoltage(0);
+  }
 
   @Override
   public void periodic() {
