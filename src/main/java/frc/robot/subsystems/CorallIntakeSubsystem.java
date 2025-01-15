c98be284c84c3a53f10f46d836202846f4a3c988
John Carlo Villanueva <johncarlovillanueva2008@gmail.com>
2025-01-14 15:49:06 -1000

    making coral and algae cmd and subsystem

---
+++ src/main/java/frc/robot/subsystems/CorallIntakeSubsystem.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class CorallIntakeSubsystem extends SubsystemBase {
+  /** Creates a new CorallIntakeSubsystem. */
+  public CorallIntakeSubsystem() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
