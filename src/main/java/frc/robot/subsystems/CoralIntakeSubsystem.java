96da5850f9b65f7bc554f518af29ac03fed49617
John Carlo Villanueva <johncarlovillanueva2008@gmail.com>
2025-01-15 15:36:05 -1000

    added coralswitch motor

--- src/main/java/frc/robot/subsystems/CoralIntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralIntakeSubsystem.java
@@ -1,15 +1,19 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class CoralIntakeSubsystem extends SubsystemBase {
   
   private final TalonFX coralIntake;
+  private final SparkMax coralSwitch;
 
   public CoralIntakeSubsystem() {
     coralIntake = new TalonFX(9);
+    coralSwitch = new SparkMax(1, MotorType.kBrushless);
   }
 
   public void intake(double speed){ // speed = amount of voltage on motor
