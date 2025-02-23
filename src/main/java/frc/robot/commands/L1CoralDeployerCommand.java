0dfb83dca13c48a6ccf45f269f1f114290d3ce55
John Carlo Villanueva <johncarlovillanueva2008@gmail.com>
2025-02-23 11:44:14 -1000

    changed L1coraldeployer to keep going while button is pressed

--- src/main/java/frc/robot/commands/L1CoralDeployerCommand.java
+++ src/main/java/frc/robot/commands/L1CoralDeployerCommand.java
@@ -32,6 +32,6 @@ public class L1CoralDeployerCommand extends Command {
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-      return !coralIntakeSub.getOpticalSensor();
+      return false;
     }
 }
